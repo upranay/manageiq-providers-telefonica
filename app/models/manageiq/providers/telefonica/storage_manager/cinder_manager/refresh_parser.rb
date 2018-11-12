8fa8f223a8945e61782a207f4a77773b000c105a
PratikAwchat <pratikawchat@gmail.com>
2018-11-12 15:22:11 +0530

    Added changes for cloud volume at the time of adding provider

--- app/models/manageiq/providers/telefonica/storage_manager/cinder_manager/refresh_parser.rb
+++ app/models/manageiq/providers/telefonica/storage_manager/cinder_manager/refresh_parser.rb
@@ -1,4 +1,43 @@
 module ManageIQ::Providers
   class Telefonica::StorageManager::CinderManager::RefreshParser < ManageIQ::Providers::StorageManager::CinderManager::RefreshParser
+
+    def self.ems_inv_to_hashes(ems, options = nil)
+      new(ems, options).ems_inv_to_hashes
+    end
+
+    def initialize(ems, options = nil)
+      @ems               = ems
+      @connection        = ems.connect
+      @options           = options || {}
+
+      @cinder_service    = ems.parent_manager&.cinder_service
+    end
+
+    def ems_inv_to_hashes
+      get_volumes
+    end
+
+    def volumes
+      @volumes ||= @cinder_service&.handled_list(:volumes)
+    end
+
+    def parse_volume(volume)
+      uid = volume.id
+      new_result = {
+          :ems_ref       => uid,
+          :type          => "ManageIQ::Providers::Telefonica::CloudManager::CloudVolume",
+          :name          => volume_name(volume).blank? ? volume.id : volume_name(volume),
+          :status        => volume.status,
+          :bootable      => volume.attributes['bootable'],
+          :description   => volume_description(volume),
+          :volume_type   => volume.volume_type,
+          :snapshot_uid  => volume.snapshot_id,
+          :size          => volume.size.to_i.gigabytes,
+
+          # Temporarily add the object from the API to the hash - for the cross-linkers.
+          :api_obj       => volume
+      }
+      return uid, new_result
+    end
   end
 end
