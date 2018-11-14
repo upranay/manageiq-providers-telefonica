234a776cfa867f235dd043119be2d453081a432d
PratikAwchat <pratikawchat@gmail.com>
2018-11-14 12:15:11 +0530

    Added some fixtures to update storage service

--- app/models/manageiq/providers/telefonica/storage_manager/cinder_manager/refresh_parser.rb
+++ app/models/manageiq/providers/telefonica/storage_manager/cinder_manager/refresh_parser.rb
@@ -5,20 +5,12 @@ module ManageIQ::Providers
       new(ems, options).ems_inv_to_hashes
     end
 
-    def initialize(ems, options = nil)
-      @ems               = ems
-      @connection        = ems.connect
-      @options           = options || {}
-
-      @cinder_service    = ems.parent_manager&.cinder_service
-    end
-
     def ems_inv_to_hashes
       get_volumes
     end
 
-    def volumes
-      @volumes ||= @cinder_service&.handled_list(:volumes)
+    def get_volumes
+      process_collection(volumes, :cloud_volumes) { |volume| parse_volume(volume) }
     end
 
     def parse_volume(volume)
@@ -33,7 +25,6 @@ module ManageIQ::Providers
           :volume_type   => volume.volume_type,
           :snapshot_uid  => volume.snapshot_id,
           :size          => volume.size.to_i.gigabytes,
-          # Temporarily add the object from the API to the hash - for the cross-linkers.
           :api_obj       => volume
       }
       return uid, new_result
